fc6228fa438642d6310bea750c93a25227843239
Louis-He <1726110778@qq.com>
2017-10-31 09:04:03 -0400

    project updated

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-import urllib
+from datetime import datetime
+import time
+import urllib.request
+from urllib.request import urlretrieve,urlopen
+import os
+
+utc = -4
+
+# return the URL of the latest GFS model (4 hours after the initial)
+def decideURL(forecasthour):
+    global utc
+    hourresult = ''
+
+    timechange = -1 * utc
+    nowtime = time.time() + timechange * 60 * 60 # struct time of UTC
+    result = time.strftime("%H", time.localtime(nowtime))  # hour of UTC time
+    print(result)
+
+    if int(result) >= 22:
+        hourresult = '18'
+    elif int(result) < 4:
+        hourresult = '18'
+        nowtime = nowtime - 24 * 60 * 60
+    elif int(result) < 10:
+        hourresult = '00'
+    elif int(result) < 16:
+        hourresult = '06'
+    elif int(result) < 22:
+        hourresult = '12'
+
+    #http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t12z.pgrb2.0p25.f000&all_var=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.2017102712
+
+    result = time.strftime("%Y%m%d"+hourresult, time.localtime(nowtime))  # hour of UTC time
+    filename = result
+    result = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t'+ hourresult +'z.pgrb2.0p25.f' + forecasthour + '&all_var=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.' + result
+    return [result, 'gfs.'+filename]
+
+#定义下载函数downLoadPicFromURL（本地文件夹，网页URL）
+def downLoadPicFromURL(dest_dir,URL):
+        try:
+          urllib.request.urlopen(URL , dest_dir)
+        except:
+          print ('\tError retrieving the URL:', URL)
+
+
+#set the path of the file from GFS
+downloadhour = ['000','024','048','072','096','120']
+total = len(downloadhour)
+count = 0
+for i in downloadhour:
+    count += 1
+    downloadinfo = decideURL(i)
+    print('[' + str((count-1)/total) + '%]Dowanloading file... from URL: ' + downloadinfo[0])
+    path = 'rawfile/' + downloadinfo[1]
+    urlretrieve(downloadinfo[0], path)
---
+++ plot.py
@@ -0,0 +1,57 @@
+import pygrib
+
+from mpl_toolkits.basemap import Basemap, cm
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import numpy as np
+
+grbs = pygrib.open('rawfile/gfs.2017103012')
+grb = grbs.select(name='Apparent temperature')[0]
+maxt = grb.values.T
+lats, lons = grb.latlons()
+
+
+maxt, lats, lons = grb.data(lat1=-90,lat2=90,lon1=0,lon2=359.75)
+maxt2=[]
+for i in range(maxt.shape[0]):
+    maxt2.append(maxt[maxt.shape[0]-i-1])
+
+maxt = np.array(maxt2)
+#print(maxt)
+
+# create figure and axes instances
+fig = plt.figure(figsize=(11,7))
+ax = fig.add_axes([0.1,0.1,0.8,0.8])
+'''
+m = Basemap(llcrnrlon=0., llcrnrlat=-90., urcrnrlon=359.75, urcrnrlat=90., \
+              rsphere=(6378137.00, 6356752.3142), \
+              resolution='l', projection='merc', \
+              lat_0=0.25, lon_0=0.25, lat_ts=20.)
+'''
+m = Basemap(llcrnrlon=0., llcrnrlat=-90., urcrnrlon=359.75, urcrnrlat=90.)
+#m = Basemap(projection='robin',lon_0=0,resolution='c')
+# draw coastlines, state and country boundaries, edge of map.
+m.drawcoastlines()
+m.drawstates()
+m.drawcountries()
+# draw parallels.
+parallels = np.arange(-90.,90.,20.)
+m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
+# draw meridians
+meridians = np.arange(0.,360.,20.)
+m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
+
+ny = maxt.shape[0]; nx = maxt.shape[1]
+lons, lats = m.makegrid(nx, ny) # get lat/lons of ny by nx evenly space grid.
+x, y = m(lons, lats) # compute map proj coordinates.
+
+# draw filled contours.
+clevs = [-40,-36,-32,-28,-24,-20,-16,-12,-8,-4,0,4,8,12,16,20,24,28,32,36,40]
+cs = m.contourf(x,y,maxt - 273.15,clevs,cmap=cm.GMT_haxby)
+# add colorbar.
+cbar = m.colorbar(cs,location='bottom',pad="5%")
+cbar.set_label('°C')
+# add title
+plt.title('example plot: 201703012z global 2m apparent Temprature (°C)')
+
+plt.show()
