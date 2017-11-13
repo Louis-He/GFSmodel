e6bcd9ee71f4a284add0f8cdc2c66b9a038ad12f
Louis-He <1726110778@qq.com>
2017-11-13 18:47:23 -0500

    update

--- process.py
+++ process.py
@@ -10,29 +10,13 @@ def createsh():
     f.close()
     print('[' + time.strftime('%Y-%m-%d %H:%M:%S',
                               time.localtime(time.time() + utc * 60 * 60)) + ']' + 'Write script file...')
+    f = open('/root/GFS/plot.sh', 'w+')
+    f.close()
     f = open('/root/GFS/process.sh', 'w+')
     f.write(
         'python3 main.py\npython3 productinitialize.py\n')
     f.close()
-    path = 'rawfile/'
-    files = os.listdir(path)
-    print(files)
-    for file in files:
-        if file[0:3] == 'gfs':
-            try:
-                f = open('/root/GFS/process.sh', 'a+')
-                f.write(
-                    'python3 plotWTP.py --path ' + file + '\n')
-                f.close()
-            except:
-                print('[FATAL ERR] PROGRAM STOP' + file)
-                f = open('/root/GFS/sysreport/errreport.txt', 'a+')
-                f.write(
-                    '[' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + utc * 60 * 60)) + ']' + '\t' +
-                    file + ' SCRIPT INITIALIZE FAILED! PLEASE CHECK\n')
-                f.close()
-    print('[' + time.strftime('%Y-%m-%d %H:%M:%S',
-                              time.localtime(time.time() + utc * 60 * 60)) + ']' + 'Script Done. Ready to run.')
+
 
 def start():
     print('[' + time.strftime('%Y-%m-%d %H:%M:%S',
