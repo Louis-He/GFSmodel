16af07fcbad4fc0b1676691c9f93ede578e8f213
Louis-He <1726110778@qq.com>
2017-11-13 01:23:08 -0500

    update

--- test2base.py
+++ test2base.py
@@ -1,4 +1,3 @@
-
 import pygrib
 
 from mpl_toolkits.basemap import Basemap, cm, shiftgrid
@@ -182,3 +181,5 @@ def plot():
 
     #GFS 10m Wind and 2m Air Temperature\nlnit:00z Nov 04 2017 Forecast Hour[36] valid at 12z Sun,Nov 05 2017 6-hour #ERA Interim 850hpa Wind speed and Temperature & 500hpa Geopotential Height#Streamlines
     plt.savefig('gfs.png', bbox_inches='tight')
+
+plot()
